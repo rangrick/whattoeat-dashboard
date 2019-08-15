e3e5b1981d71a3d31b3ebc4677d57f17ebad03c1
rfeuerabend <rfeuerabend@team.mobile.de>
2019-08-15 17:00:05 +0200

    cache and collect places

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const TextSearch = require("googleplaces/lib/TextSearch.js")
 const config = require("./config/config.js")
 const textSearch = new TextSearch(config.apiKey, config.outputFormat)
+var fs = require("fs")
+const restaurants = require("./restaurants.json")
 
 var express = require("express")
 var app = express()
@@ -16,6 +18,51 @@ app.get("/places", function(req, res) {
     }
   )
 })
+
+app.get("/placesCached", function(req, res) {
+  res.send(restaurants)
+})
+
+app.get("/updatePlaces", function(req, res) {
+  var results = []
+  var count = 0
+  const search = query =>
+    textSearch(query, function(error, response) {
+      const token = response.next_page_token
+
+      if (response.results) {
+        results = [...results, ...response.results]
+      }
+
+      if (token && count < 20) {
+        console.log("Start NEW search number: ", count)
+        count = count++
+
+        setTimeout(
+          () =>
+            search({
+              query: "Jena restaurant",
+              pagetoken: token
+            }),
+          10000
+        )
+      } else {
+        write(results)
+        console.log("Restaurants updated")
+      }
+    })
+
+  search({
+    query: "Jena restaurant"
+  })
+})
+
+const write = results => {
+  let outputFile = "./restaurants.json"
+  fs.writeFile(outputFile, JSON.stringify(results), {}, () =>
+    console.log("file written to " + outputFile)
+  )
+}
 
 app.listen(port, function() {
   console.log("App listening on port " + port + "!")
--- src/App.js
+++ src/App.js
@@ -2,37 +2,25 @@ import React, { useEffect, useState } from "react"
 import "./App.css"
 
 import Charts from "./compoenents/charts"
-import List from "./compoenents/list"
-import Search from "./compoenents/search"
 
 function App() {
   const [data, setData] = useState(false)
-  const fetchData = ({ token, search }) => {
-    fetch(
-      `places?search=${search}` + (token ? `&next_page_token=${token}` : "")
-    )
+  const fetchData = ({ search }) => {
+    fetch("placesCached")
       .then(response => response.json())
       .then(newData => {
+        console.log(newData)
         setData(newData)
-
-        if (newData.next_page_token) {
-          // fetchData(newData.next_page_token)
-        }
       })
   }
   useEffect(() => {
     fetchData({ search: "Jena restaurant" })
   }, [])
 
-  const dispatchSearch = search => {
-    fetchData({ search })
-  }
-
   return (
     <div className="App">
-      <Search dispatchSearch={dispatchSearch} />
-
-      <Charts data={data.results} />
+      <Randomize />
+      <Charts data={data} />
     </div>
   )
 }
